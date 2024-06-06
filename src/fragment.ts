9284443e9f29874cdf32c71f63c7940a4e6a7a69
Seva Zaikov <seva.zaikov@gmail.com>
2024-06-05 20:19:05 -0700

    make Fragment more reliable in useValue

--- src/fragment.ts
+++ src/fragment.ts
@@ -2,7 +2,7 @@ import { createElement } from "./create-element";
 
 import type { VelesChildren } from "./types";
 
-function Fragment({ children }: { children: VelesChildren }) {
+function Fragment({ children }: { children?: VelesChildren }) {
   return createElement("div", {
     phantom: true,
     children,
