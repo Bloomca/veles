6a96295fa2a5834c037d22d83509c449e897a5c7
Seva Zaikov <seva.zaikov@gmail.com>
2026-03-29 13:51:18 -0700

    Fix building script (#92)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts", "src/jsx-runtime.ts"],
   format: ["esm", "cjs"],
   sourcemap: false,
   clean: true,
+  fixedExtension: false,
   dts: true,
 });
