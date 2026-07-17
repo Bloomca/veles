a180a1dd0ee85d7d346c760543c405e8a6803a52
Seva Zaikov <seva.zaikov@gmail.com>
2026-07-17 09:26:43 -0700

    fix conditional rendering with empty fragments (#120)

--- src/fragment.ts
+++ src/fragment.ts
@@ -1,12 +1,22 @@
 import { createElement } from "./create-element";
+import { createTextElement } from "./create-element/create-text-element";
 
 import type { VelesChildren } from "./types";
 
 function Fragment({ children }: { children?: VelesChildren }) {
-  return createElement("div", {
+  const fragment = createElement("div", {
     phantom: true,
     children,
   });
+
+  if (fragment.childComponents.length === 0) {
+    const anchor = createTextElement("");
+    anchor.parentVelesElement = fragment;
+    fragment.childComponents.push(anchor);
+    fragment.html.append(anchor.html);
+  }
+
+  return fragment;
 }
 
 export { Fragment };
